87480bfcfcd5b2c0e96c9cba022028ff1d289092
Viktor De Pasquale <info@depasquale.wiki>
2022-03-02 18:31:14 +0100

    Fixed key being 0 multiple times

--- gecko-android/src/main/java/gecko/android/tooling/Paging.kt
+++ gecko-android/src/main/java/gecko/android/tooling/Paging.kt
@@ -3,7 +3,7 @@ package gecko.android.tooling
 import androidx.paging.PagingSource
 
 internal fun PagingSource.LoadParams<Int>.prevKey() =
-    key?.minus(loadSize)?.coerceAtLeast(0)
+    key?.minus(loadSize)?.coerceAtLeast(0)?.takeUnless { it == 0 }
 
 internal fun PagingSource.LoadParams<Int>.nextKey(list: List<*>) =
     ((key ?: 0) + loadSize).takeUnless { list.isEmpty() }
