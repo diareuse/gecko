2d09a610632af3f719bc511d580e57a19fdf3245
Viktor De Pasquale <info@depasquale.wiki>
2022-03-07 15:42:44 +0100

    Added new ways of launching gecko
    Gecko will now try to launch primarily from a shortcut. If it cannot create one or one does not exist, then it enables full on launcher icon.

---
+++ gecko-android-ui/src/main/java/gecko/ui/startup/GeckoStartup.kt
@@ -0,0 +1,66 @@
+package gecko.ui.startup
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
+import androidx.startup.Initializer
+import gecko.ui.GeckoActivity
+import gecko.ui.R
+
+class GeckoStartup : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        var hasAlternativeLauncher: Boolean = context.hasShortcut
+        hasAlternativeLauncher = hasAlternativeLauncher || context.tryAddShortcut()
+        context.setLauncherEnabled(!hasAlternativeLauncher)
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return emptyList()
+    }
+
+    // ---
+
+    private val Context.hasShortcut
+        get() = ShortcutManagerCompat
+            .getShortcuts(this, ShortcutManagerCompat.FLAG_MATCH_DYNAMIC)
+            .any { it.id == "gecko" }
+
+    private fun Context.tryAddShortcut(): Boolean {
+        val intent = Intent(Intent.ACTION_MAIN)
+            .setClass(this, GeckoActivity::class.java)
+        val shortcut = ShortcutInfoCompat.Builder(this, "gecko")
+            .setShortLabel("Gecko!")
+            .setLongLabel("Gecko! Network Logging")
+            .setIcon(IconCompat.createWithResource(this, R.mipmap.ic_launcher_gecko))
+            .setIntent(intent)
+            .setRank(0)
+            .build()
+
+        return try {
+            ShortcutManagerCompat.pushDynamicShortcut(this, shortcut)
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+            // launcher activity does not exist
+            false
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+            // user has defined this shortcut as immutable
+            false
+        }
+    }
+
+    private fun Context.setLauncherEnabled(enabled: Boolean) {
+        val component = ComponentName(this, "gecko.ui.GeckoLauncher")
+        val state = when (enabled) {
+            true -> PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+            false -> PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+        }
+        packageManager.setComponentEnabledSetting(component, state, PackageManager.DONT_KILL_APP)
+    }
+
+}
