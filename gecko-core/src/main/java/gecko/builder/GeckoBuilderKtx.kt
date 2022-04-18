91c298756ba5d9d9716fac3438963e568a7db54e
Viktor De Pasquale <info@depasquale.wiki>
2022-04-18 10:31:45 +0200

    Fixed initializing encode before using it
    This could have caused crashes on android devices <26

--- gecko-core/src/main/java/gecko/builder/GeckoBuilderKtx.kt
+++ gecko-core/src/main/java/gecko/builder/GeckoBuilderKtx.kt
@@ -7,14 +7,18 @@ import gecko.MetadataAdapterAscii
 import java.util.*
 
 inline fun gecko(builder: GeckoConfiguration.() -> Unit = {}): Gecko {
-    return GeckoBuilder().apply(::geckoDefaults).apply(builder).build()
+    return GeckoBuilder().apply(builder).apply(::geckoDefaults).build()
 }
 
 @PublishedApi
 internal fun geckoDefaults(configuration: GeckoConfiguration) {
-    val encoder = Base64.getUrlEncoder()
     configuration.domain = "diareuse.github.io/gecko"
-    configuration.encoder = Base64Encoder { encoder.encode(it) }
+    configuration.encoder = object : Base64Encoder {
+        val encoder = Base64.getUrlEncoder()
+        override fun encode(data: ByteArray): ByteArray {
+            return encoder.encode(data)
+        }
+    }
     configuration.adapter = MetadataAdapterAscii()
     configuration.logger = Logger { println(it) }
 }
