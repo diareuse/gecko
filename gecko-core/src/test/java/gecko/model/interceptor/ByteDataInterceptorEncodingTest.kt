e89da6e47253daaa00d436dfbcb707af539dc9b2
Viktor De Pasquale <info@depasquale.wiki>
2022-08-17 08:07:20 +0200

    Added encoding test

---
+++ gecko-core/src/test/java/gecko/model/interceptor/ByteDataInterceptorEncodingTest.kt
@@ -0,0 +1,31 @@
+package gecko.model.interceptor
+
+import com.google.common.truth.Truth.assertThat
+import gecko.model.ByteData
+import gecko.model.ByteDataStage
+import gecko.model.decorator.ByteDataBase64Decorator
+import gecko.test.TestBlueprint
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ByteDataInterceptorEncodingTest : TestBlueprint {
+
+    private lateinit var interceptor: ByteDataInterceptorEncoding
+
+    @BeforeEach
+    override fun prepare() {
+        super.prepare()
+        interceptor = ByteDataInterceptorEncoding()
+    }
+
+    @Test
+    fun getStage() {
+        assertThat(interceptor.stage).isEqualTo(ByteDataStage.Encoding)
+    }
+
+    @Test
+    fun intercept() {
+        assertThat(interceptor.intercept(ByteData.from(""))).isInstanceOf(ByteDataBase64Decorator::class.java)
+    }
+
+}
