af34eab1c0b1d1373503ba4266293963deee37d2
Viktor De Pasquale <info@depasquale.wiki>
2022-08-17 08:04:41 +0200

    Added domain injecting test

---
+++ gecko-core/src/test/java/gecko/model/interceptor/ByteDataInterceptorDomainInjectingTest.kt
@@ -0,0 +1,32 @@
+package gecko.model.interceptor
+
+import com.google.common.truth.Truth.assertThat
+import gecko.model.ByteData
+import gecko.model.ByteDataStage
+import gecko.test.TestBlueprint
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ByteDataInterceptorDomainInjectingTest : TestBlueprint {
+
+    private lateinit var interceptor: ByteDataInterceptorDomainInjecting
+
+    @BeforeEach
+    override fun prepare() {
+        super.prepare()
+        interceptor = ByteDataInterceptorDomainInjecting()
+    }
+
+    @Test
+    fun `matches stage`() {
+        assertThat(interceptor.stage).isEqualTo(ByteDataStage.PostProcessing)
+    }
+
+    @Test
+    fun `returns url data`() {
+        val query = ByteData.from("query")
+        val result = interceptor.intercept(query)
+        assertThat(result.value.toString()).isEqualTo("https://diareuse.github.io/gecko/?q=query")
+    }
+
+}
