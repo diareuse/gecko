60ef8276e1a902a8769f16a0d540d48addc012fa
Viktor De Pasquale <hello@depasquale.wiki>
2024-10-29 07:53:18 +0100

    Refactor: Handle Android 13 Notification Permission
    Handle Android 13's new notification permission (`POST_NOTIFICATIONS`) by only showing the notification if the permission is granted.
    If the permission is not granted or on devices below Android 13, the notification is displayed as before.
    
    Additionally, updated the notification to set the `PendingIntent` only if the target activity (`gecko.ui.GeckoActivity`) exists.

--- gecko-android-notification/src/main/java/gecko/android/notification/NotificationDispatcherDefault.kt
+++ gecko-android-notification/src/main/java/gecko/android/notification/NotificationDispatcherDefault.kt
@@ -2,8 +2,14 @@ package gecko.android.notification
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.NameNotFoundException
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
@@ -13,6 +19,8 @@ class NotificationDispatcherDefault(
     private val context: Context
 ) : NotificationDispatcher {
 
+    private val intent = Intent()
+        .setComponent(ComponentName(context.packageName, "gecko.ui.GeckoActivity"))
     private val notificationText = NotificationCompat.BigTextStyle()
         .setBigContentTitle("Gecko!")
         .bigText("")
@@ -38,11 +46,25 @@ class NotificationDispatcherDefault(
         notification.setStyle(notificationText)
         notification.setProgress(0, 0, false)
         notification.setContentText("Expand to view requests")
-        if (hasPermission())
+        notification.setContentIntent(resolvePendingIntent())
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU || hasPermission())
             manager.notify(1337, notification.build())
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun hasPermission(permission: String = Manifest.permission.POST_NOTIFICATIONS) =
         checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+
+    private fun exists() = try {
+        context.packageManager.queryIntentActivities(intent, 0).size > 0
+    } catch (e: NameNotFoundException) {
+        false
+    }
+
+    private fun resolvePendingIntent() = if (exists()) {
+        PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+    } else {
+        null
+    }
 
 }
