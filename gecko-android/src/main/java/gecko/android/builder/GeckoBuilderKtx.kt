b76c79c45c1bd7b84f95b430ab104493c8124ab4
Viktor De Pasquale <hello@depasquale.wiki>
2024-10-29 07:53:35 +0100

    Fixed deprecated callable
    Added suppress warnings for deprecated callable.

--- gecko-android/src/main/java/gecko/android/builder/GeckoBuilderKtx.kt
+++ gecko-android/src/main/java/gecko/android/builder/GeckoBuilderKtx.kt
@@ -1,9 +1,12 @@
+@file:Suppress("DEPRECATION")
+
 package gecko.android.builder
 
 import android.content.Context
 import gecko.Gecko
 import gecko.builder.GeckoConfiguration
 
+@Suppress("DeprecatedCallableAddReplaceWith")
 @Deprecated("Add steps via @AutoService annotated class")
 inline fun Context.geckoAndroid(builder: GeckoConfiguration.() -> Unit = {}): Gecko {
     return Gecko
